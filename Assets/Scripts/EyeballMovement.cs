6e327a860add66aafe7f63d5b51558a578294989
Mohammad Firdausi <mfirda1@lsu.edu>
2015-01-24 01:11:41 -0600

    Make the eyeball move

---
+++ Assets/Scripts/EyeballMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeballMovement : MonoBehaviour {
+	private Vector3 mousePosition;
+	public float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		moveSpeed = 10f;
+		mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		transform.position = Vector2.Lerp (transform.position, mousePosition, moveSpeed);
+
+//		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
+//		rot *= Quaternion.Euler (0, 0, 90);
+//
+//		if ((transform.position.x - mousePosition.x) > 0) {
+//			transform.localScale = new Vector3 (.25f, -.25f, .25f);
+//		}
+//		if ((transform.position.x - mousePosition.x) < 0){
+//			transform.localScale = new Vector3(.25f, .25f, .25f);
+//		}
+//		if ((transform.position.x == mousePosition.x) && (transform.position.y == mousePosition.y)) {
+//			rigidbody2D.velocity = Vector3.zero;
+//			rigidbody2D.angularVelocity = 0f;
+//		}
+//		//transform.rotation = rot;
+//		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+//		rigidbody2D.AddForce (gameObject.transform.right * moveSpeed);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		Debug.Log ("Hit ");
+
+	}
+}
