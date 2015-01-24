4159aa002f3f29c42536f76c330561c2f06cf6dc
rmoon3 <rmoon3@lsu.edu>
2015-01-23 23:28:01 -0600

    Keyboard Control v0.1
    Added first version of character controller

---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour {
+	public float speed = 5.0f;
+	public float gravity = 2f;
+	public float cutJumpSpeedLimit = 10f;
+	private bool onGround = false;
+	private int m_TerrainLayer;
+	//private Vector2 amtToMove;
+
+	
+	void Start () 
+	{
+		float cutJumpSpeed = gravity / cutJumpSpeedLimit;
+		m_TerrainLayer = LayerMask.NameToLayer("Terrain");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		//Constant movement right
+		transform.Translate (Vector2.right * speed * Time.deltaTime);
+		//Gravity Logic
+		if (onGround == false) 
+		{
+			transform.Translate (-Vector2.up * gravity * Time.deltaTime);
+		}
+		//jump logic
+
+	}
+	void OnCollisionrEnter2D(Collider2D coll)
+	{
+		//Controls 
+		if (coll.gameObject.layer == m_TerrainLayer)
+		{
+			Debug.Log("Touching Ground");
+			onGround = true;
+			transform.Translate(Vector2.up * 0);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+	
+	// Player Handling
+	public float gravity = 20;
+	public float walkSpeed = 8;
+	public float runSpeed = 12;
+	public float acceleration = 30;
+	public float jumpHeight = 12;
+	public float cutJumpSpeed = 10;
+
+	private float animationSpeed;
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+	private bool isJumping;
+	private float cutJumpSpeedLimit;
+
+	
+	private PlayerPhysics playerPhysics;
+	//private Animator animator;
+	
+
+	void Start () {
+		playerPhysics = GetComponent<PlayerPhysics>();
+		cutJumpSpeedLimit = gravity / cutJumpSpeed;
+		//animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		// Reset acceleration upon collision
+		if (playerPhysics.movementStopped) {
+			targetSpeed = 0;
+			currentSpeed = 0;
+		}
+		
+		// If player is touching the ground
+		if (playerPhysics.grounded) {
+			amountToMove.y = 0;
+			isJumping = false;
+			
+			// Jump
+			if (Input.GetButtonDown("Jump")) {
+				amountToMove.y = jumpHeight;
+				isJumping = true;
+			}
+
+		}
+		if (Input.GetButtonUp("Jump") && isJumping && amountToMove.y > cutJumpSpeedLimit) 
+		{
+			Debug.Log("Entered");
+			amountToMove.y = cutJumpSpeedLimit;
+		}
+		
+		// Set animator parameters
+		//animationSpeed = IncrementTowards(animationSpeed,Mathf.Abs(targetSpeed),acceleration);
+		//animator.SetFloat("Speed",animationSpeed);
+		
+		// Input
+		float speed = (Input.GetButton("Run"))?runSpeed:walkSpeed;
+		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+		currentSpeed = IncrementTowards(currentSpeed, targetSpeed,acceleration);
+		
+		// Set amount to move
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move(amountToMove * Time.deltaTime);
+		
+		// Face Direction
+		float moveDir = Input.GetAxisRaw("Horizontal");
+		if (moveDir !=0) {
+			transform.eulerAngles = (moveDir>0)?Vector3.up * 180:Vector3.zero;
+		}
+
+	}
+	
+	// Increase n towards target by speed
+	private float IncrementTowards(float n, float target, float a) {
+		if (n == target) {
+			return n;	
+		}
+		else {
+			float dir = Mathf.Sign(target - n); // must n be increased or decreased to get closer to target
+			n += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target-n))? n: target; // if n has now passed target then return target, otherwise return n
+		}
+	}
+}
