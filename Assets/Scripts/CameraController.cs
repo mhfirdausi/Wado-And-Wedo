3c757a41857d0c6442d8c6f13097ae3b7007983a
rmoon3 <rmoon3@lsu.edu>
2015-01-24 10:08:32 -0600

    Add few level prefabs and camera physics

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,16 +2,17 @@
 using System.Collections;
 
 public class CameraController: MonoBehaviour {
-
+	public float distanceBehindPlayer;
+	public float distanceAbovePlayer;
 	public GameObject player;
 	// Use this for initialization
 	void Start () {
-	
+		transform.position = new Vector3 (player.transform.position.x + distanceBehindPlayer , player.transform.position.y + distanceAbovePlayer, -10);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, -10);
+		transform.position = new Vector3 (player.transform.position.x + distanceBehindPlayer , transform.position.y, -10);
 	
 	}
 }
