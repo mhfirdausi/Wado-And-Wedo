69ea9eb163db7ae24d30f1ccc764f26bc7059456
rmoon3 <rmoon3@lsu.edu>
2015-01-25 13:40:05 -0600

    Fixed all menus

---
+++ Assets/doorTwoManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorTwoManager : MonoBehaviour {
+	public GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider collision){
+		//Debug.Log ("Collided");
+		if (collision.gameObject.layer == 9)
+		{
+			gameManager.endScreenTwo();
+		}
+	}
+}
