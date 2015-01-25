1e55cef10427f81e22810cf7066d86a3e3b8cd32
Mohammad Firdausi <mfirda1@lsu.edu>
2015-01-25 10:26:33 -0600

    now sound doesnt work

---
+++ Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter (Collider coll) {
+		if (coll.gameObject.layer == 9) {
+			Debug.Log("Wado has won!");
+//			Application.LoadLevel(3);
+		}
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -61,6 +61,7 @@ public class GameManager : MonoBehaviour {
 	public void startGame()
 	{
 		Application.LoadLevel(1);
+		audio.Play();
 	}
 	public void quit()
 	{
